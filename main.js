502deb6bdbe35118df488c1d94e9c7d2720890aa
onyekachi <emegwaonyeka@gmail.com>
2023-01-15 15:46:59 +0100

    end of developing

---
+++ main.js
@@ -0,0 +1,4 @@
+const showAccordion = document.querySelectorAll('.add');
+const closeAccordion = document.querySelectorAll('.close');
+
+showAccordion.add
---
+++ user.js
@@ -0,0 +1,18 @@
+
+
+const menuBtn = document.querySelector('.menu');
+
+const menuList = document.querySelector('.nav-links-container');
+
+
+// console.log(menuBtn)
+// console.log(menuList)
+
+menuBtn.addEventListener("click", function(){
+    menuList.classList.toggle("show")
+})
+
+
+
+
+
